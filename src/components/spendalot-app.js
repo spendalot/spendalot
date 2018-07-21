c7037c7ae3170c8ee536936efee9ae3e1b851723
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-07-21 15:23:51 +0800

    💥 This is just a side project, refactored to use CSS Grid instead

--- src/components/spendalot-app.js
+++ src/components/spendalot-app.js
@@ -11,6 +11,7 @@ import '@polymer/app-layout/app-toolbar/app-toolbar.js';
 
 import { installRouter } from 'pwa-helpers/router.js';
 import { installOfflineWatcher } from 'pwa-helpers/network.js';
+import { installMediaQueryWatcher } from 'pwa-helpers/media-query.js';
 
 import '@polymer/paper-icon-button/paper-icon-button.js';
 
@@ -31,9 +32,17 @@ class SpendalotApp extends connectStore(LitElement) {
     return html`
     <style>
       :host {
-        display: block;
+        display: grid;
+        grid-template-columns: minmax(auto, var(--spendalot-app-main-max-width)) minmax(auto, var(--spendalot-app-trend-max-width));
+        grid-template-rows: minmax(auto, 150px) minmax(auto, 1fr) 64px;
+        grid-template-areas: "header trend"
+                             "main trend"
+                             "bottomsheet trend";
+
         box-sizing: border-box;
 
+        --spendalot-app-main-max-width: 80%;
+        --spendalot-app-trend-max-width: 20%;
         --spendalot-app-primary-color: #2779ff;
         --spendalot-app-secondary-color: #fff;
         --spendalot-app-primary-border-radius: 20px;
@@ -44,13 +53,10 @@ class SpendalotApp extends connectStore(LitElement) {
         box-sizing: border-box;
       }
 
-
       header {
-        display: flex;
-        flex-direction: column;
+        grid-area: header;
 
         width: 100%;
-        height: 100%;
       }
       header::before {
         display: content;
@@ -84,6 +90,8 @@ class SpendalotApp extends connectStore(LitElement) {
       }
 
       main {
+        grid-area: main;
+
         min-height: 100vh;
         width: 100%;
         height: 100%;
@@ -109,9 +117,55 @@ class SpendalotApp extends connectStore(LitElement) {
         height: 100vh;
       }
 
-      .bottomsheet-nav {
+      .nav-trend {
+        grid-area: trend;
+
+        display: grid;
+        grid-template-columns: 16px 1fr 16px;
+        grid-template-rows: 64px minmax(auto, 1fr);
+        grid-template-areas: ". header ."
+                             ". trend .";
+
+        background-color: #30517a;
+      }
+      .nav-trend__title {
+        grid-area: header;
+        align-self: center;
+        justify-self: center;
+
+        color: #6eea82;
+        font-size: 24px;
+        font-weight: 500;
+        letter-spacing: 2px;
+      }
+
+      .nav-trend__trend-card-container {
+        grid-area: trend;
+
+        margin: 24px 0 0;
+      }
+      .trend-card {
+        margin: 0 0 24px;
+      }
+      .trend-card__title {
+        color: #fff;
+      }
+      .trend-card__content {
+        height: 300px;
+        background-color: #fff;
+        border-radius: var(--spendalot-app-primary-border-radius);
+      }
+
+      .nav-bottomsheet {
+        grid-area: bottomsheet;
+
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        max-width: var(--spendalot-app-main-max-width);
         width: 100%;
         height: 64px;
+        padding: 0 16px;
         background-color: var(--spendalot-app-secondary-color);
         color: var(--spendalot-app-primary-color);
         border-radius: var(--spendalot-app-primary-border-radius) var(--spendalot-app-primary-border-radius) 0 0;
@@ -135,9 +189,21 @@ class SpendalotApp extends connectStore(LitElement) {
         <div class="trend-graph"></div>
         <div class="data-table"></div>
       </div>
+
     </main>
 
-    <nav class="bottomsheet-nav">
+    <nav class="nav-trend">
+      <div class="nav-trend__title">Trends</div>
+
+      <div class="nav-trend__trend-card-container">
+        <div class="trend-card">
+          <h3 class="trend-card__title">Tue, Jan 7, 2014</h3>
+          <div class="trend-card__content"></div>
+        </div>
+      </div>
+    </nav>
+
+    <nav class="nav-bottomsheet">
 
     </nav>
 
@@ -171,6 +237,9 @@ class SpendalotApp extends connectStore(LitElement) {
     });
     installOfflineWatcher((offline) => {
       store.dispatch(updateOffline(offline));
+    });
+    installMediaQueryWatcher('(max-width: 500px)', (matches) => {
+      console.log({ matches });
     });
   }
 
